1ffe87d0531079f66442d332db3b3f41394a1def
jlallas384 <jlallas384@gmail.com>
2024-01-28 15:16:18 +0800

    working layering

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ class Queue{
     constructor(list){
         this.head = null;
         this.tail = null;
-        for(let elem in list){
-            push(elem)
+        for(let elem of list){
+            this.push(elem)
         }
     }
     push(data){
@@ -48,8 +48,9 @@ class Position{
 }
 
 const N = 11
+const directions = [new Position(0, 1), new Position(1, 0), new Position(0, -1), new Position(-1, 0)]
 
-const bfs = (starts, end) => {
+const bfs = (starts, end, bombs) => {
     let queue = new Queue(starts)
 
     let dist = new Array(N)
@@ -63,9 +64,11 @@ const bfs = (starts, end) => {
         }
     }
 
-    dist[start.row][start.col] = 0
+    for(let {row, col} of starts){
+        dist[row][col] = 0
+    }
 
-    const directions = [new Position(-1, 0), new Position(1, 0), new Position(0, -1), new Position(0, 1)]
+
 
     let ret = {}
     while(!queue.empty()){
@@ -74,9 +77,10 @@ const bfs = (starts, end) => {
 
         if(cur.equal(end)){
             let path = []
-            while(!cur.equal(start)){
+            while(!starts.some((e) => {return e.equal(cur)})){
                 path.push(cur)
-                cur = from[cur.row][cur.col]
+                let pChange = from[cur.row][cur.col]
+                cur = new Position(cur.row - pChange.row, cur.col - pChange.col)
             }
             ret.path = path.reverse()
             break
@@ -85,15 +89,16 @@ const bfs = (starts, end) => {
         directions.map((elem) => {
             return new Position(elem.row + cur.row, elem.col + cur.col)
         }).filter((elem) => {
-            return 0 <= elem.row && elem.row < N && 0 <= elem.col && elem.col < N && dist[elem.row][elem.col] == -1
+            return 0 <= elem.row && elem.row < N && 0 <= elem.col && elem.col < N && dist[elem.row][elem.col] == -1 && !bombs.some((e) => {return e.equal(elem)})
         }).forEach((elem) => {
             dist[elem.row][elem.col] = dist[cur.row][cur.col] + 1
-            from[elem.row][elem.col] = cur
+            from[elem.row][elem.col] = new Position(elem.row - cur.row, elem.col - cur.col)
             queue.push(elem)
         })
     }
 
     ret.dist = dist
+    ret.from = from
     return ret
 }
 
@@ -103,8 +108,8 @@ for(let row = 0; row < N; row++){
     rowBlock.classList.add('row', 'mb-1')
 
     for(let col = 0; col < N; col++){
-        let colBlock = document.createElement('div')
-        colBlock.classList.add('border', 'me-1', 'border-3', 'rounded', 'col', 'p-0', 'cell', 'd-flex', 'justify-content-center', 'align-items-center', 'grid-btn')
+        let colBlock = document.createElement('button')
+        colBlock.classList.add('btn', 'border', 'me-1', 'border-3', 'rounded', 'col', 'p-0', 'cell', 'd-flex', 'justify-content-center', 'align-items-center', 'grid-btn', 'bg-gradient')
         colBlock.style.aspectRatio = '1/1'
         rowBlock.appendChild(colBlock)
     }
@@ -118,52 +123,81 @@ const bombState = 3
 
 let curState = 0
 
+
+
+const btns = document.querySelectorAll(".btn-item")
+
 class Handler{
     constructor(btn, state){
         this.state = state
         btn.addEventListener('click', (function(){
-            console.log(this.state)
+            if(curState)
+                btns[curState - 1].classList.remove('active-btn-item')
+
             if(curState == this.state){
                 curState = noneState
             }else{
                 curState = this.state
             }
+
+            if(curState)
+                btns[curState - 1].classList.add('active-btn-item')
+               
+            btns[this.state - 1].firstChild.nextElementSibling.classList.add('pop') 
         }).bind(this))
     }
 }
 
-const btns = document.querySelectorAll(".zz")
-
+document.querySelectorAll('.btn-image').forEach((elem) => {
+    elem.addEventListener('animationend', function(){
+        this.classList.remove("pop")
+    })
+})
 
 const sfBtn = new Handler(btns[0], startState)
 const efBtn = new Handler(btns[1], endState)
 const bombBtn = new Handler(btns[2], bombState)
 
+
 let grid = new Array(N)
 const gridBtns = document.querySelectorAll(".grid-btn")
 
-const images = ["images/start_flag.png", "images/end_flag.png", "images/bomb.png"].map((imageName) => {
+const images = ["images/start_flag.png", "images/end_flag.png", "images/bomb.png", "images/arrow.png"].map((imageName) => {
     let image = document.createElement('img')
     image.src = imageName
-    image.style.width = '90%'
-    image.style.height = '90%'
+    image.style.width = '80%'
+    image.style.height = '80%'
+    image.classList.add('img-fluid', 'pop')
     return image
 })
 
-let starts = [], ends = null
+for(let degs of ["90deg", "180deg", "270deg"]){
+    let image = images[3].cloneNode()
+    image.style.transform = `rotate(${degs})`
+    images.push(image)
+}
 
-
-let gridBtnHandler = function(e){
-    if(grid[this.row][this.col] == noneState){
-        if(curState){
-            console.log(grid[this.row][this.col])
-            if(curState == grid[this.row][this.col]){
-                console.log(e.target)
-                e.target.removeChild(e.target.firstElement)
+class GridBtnHandler{
+    constructor(e, pos){
+        this.e = e
+        this.pos = pos
+        this.state = noneState
+        e.addEventListener('click', () => {
+            if(this.state == noneState){
+                if(curState){
+                    this.state = curState == this ? noneState : curState
+                }
             }else{
-                e.target.appendChild(images[curState - 1].cloneNode())
-                grid[this.row][this.col] = curState 
+                this.state = noneState
             }
+            this.update()
+        })
+    }
+    update(){
+        if(this.e.firstChild)
+            this.e.removeChild(this.e.firstChild)
+        if(this.state){
+            this.e.appendChild(images[this.state - 1].cloneNode())
         }
     }
 }
@@ -171,9 +205,78 @@ let gridBtnHandler = function(e){
 for(let row = 0; row < N; row++){
     grid[row] = new Array(N)
     for(let col = 0; col < N; col++){
-        grid[row][col] = noneState
         let gridBtn = gridBtns[row * N + col]
-        gridBtn.addEventListener('click', gridBtnHandler.bind(new Position(row, col)))
+        grid[row][col] = new GridBtnHandler(gridBtn, new Position(row, col))
     }
 }
 
+
+const doBfs = async () => {
+    let bombs = []
+    let starts = []
+    let ends = null
+
+    for(let row = 0; row < N; row++){
+        for(let col = 0; col < N; col++){
+            let pos = new Position(row, col)
+            switch(grid[row][col].state){
+            case startState:
+                starts.push(pos)
+                break
+            case endState:
+                ends = pos
+                break
+            case bombState:
+                bombs.push(pos)
+                break
+            }
+        }
+    }
+
+    let ret = bfs(starts, ends, bombs)
+    let layers = new Array(N * N)
+    for(let layer = 1; layer < N * N; layer++){
+        layers[layer] = []
+    }
+    for(let row = 0; row < N; row++){
+        for(let col = 0; col < N; col++){
+            if(ret.dist[row][col] >= 1){
+                layers[ret.dist[row][col]].push(new Position(row, col))
+            }
+        }
+    }
+    (async () => {
+        for(let layer = 1; layer < N * N; layer++){
+            const stop = () => {
+                return new Promise((resolve) => {
+                    setTimeout(() => {
+                        resolve()
+                    }, 300)
+                })
+            }
+
+            for(let {row, col} of layers[layer]){
+                for(let [index, elem] of directions.entries()){
+                    if(elem.equal(ret.from[row][col])){
+                        grid[row][col].state = index + 4
+                        grid[row][col].update()
+                    }
+                }
+                if(grid[row][col].pos.equal(ends)){
+                    return
+                }
+            }
+
+            await stop()
+        }
+    })()
+}
+
+const reset = () => {
+    for(let row = 0; row < N; row++){
+        for(let col = 0; col < N; col++){
+            grid[row][col].state = noneState
+            grid[row][col].update()
+        }
+    }    
+}
